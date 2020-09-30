4b7daf4418c76a27f6ff8d7075fbfb96482e9b0c
John Rothfels <john@dynasty.com>
2020-09-30 12:28:38 -0700

    from the leg of Zeus

---
+++ script/deploy-local.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+##########################################################################
+# deploy-local.sh
+#
+# Usage:
+#   ./script/deploy-local.sh [version]
+#
+##########################################################################
+
+set -e
+
+echo "Packaging and deploying local repo contents."
+echo "Make sure to npm run build first!"
+echo "================================="
+
+aws ecr get-login-password | docker login --username AWS --password-stdin 101624687637.dkr.ecr.us-west-2.amazonaws.com
+
+echo "Building local docker image"
+docker build --tag 101624687637.dkr.ecr.us-west-2.amazonaws.com/cs188:local .
+
+echo "Pushing local docker image"
+docker push 101624687637.dkr.ecr.us-west-2.amazonaws.com/cs188:local
+
+echo "Updating previous and latest"
+docker pull 101624687637.dkr.ecr.us-west-2.amazonaws.com/cs188:latest
+docker tag 101624687637.dkr.ecr.us-west-2.amazonaws.com/cs188:latest 101624687637.dkr.ecr.us-west-2.amazonaws.com/cs188:previous
+docker tag 101624687637.dkr.ecr.us-west-2.amazonaws.com/cs188:local 101624687637.dkr.ecr.us-west-2.amazonaws.com/cs188:latest
+
+echo "Pushing latest and previous"
+docker push 101624687637.dkr.ecr.us-west-2.amazonaws.com/cs188:latest
+docker push 101624687637.dkr.ecr.us-west-2.amazonaws.com/cs188:previous
+
+echo "Updating app-web"
+./script/deploy-ecs.sh cs188-app-web "local"
+
+# echo "Updating app-background"
+# ./script/deploy-ecs.sh app-background "local"
+
+echo "Updating lambda"
+# aws lambda update-function-code \
+#   --no-cli-pager \
+#   --region us-west-2 \
+#   --function-name cs188 \
+#   --zip-file fileb://./server/bundle.zip
+
+echo "DONE"
---
+++ server/src/loadtest/userScript.ts
@@ -0,0 +1,45 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { answerSurveyQuestion } from '../../../web/src/view/playground/mutateSurveys'
+
+/**
+ * A user script is a sequence of HTTP requests (REST/GraphQL API requests, page loads, etc.)
+ *
+ * Each user arriving at the site during the load test will execute the user script.
+ */
+export type UserScript = () => Promise<any>
+
+// set this is you require authenticated requests
+const authToken = undefined
+
+export async function userScript() {
+  await fetch('https://cs188.cloudcity.computer/')
+  await fetch('https://cs188.cloudcity.computer/')
+  await fetch('https://cs188.cloudcity.computer/')
+
+  await answerSurveyQuestion(makeApolloClient(), { answer: 'test answer', questionId: 1 })
+  await answerSurveyQuestion(makeApolloClient(), { answer: 'test answer', questionId: 1 })
+  await answerSurveyQuestion(makeApolloClient(), { answer: 'test answer', questionId: 1 })
+  await answerSurveyQuestion(makeApolloClient(), { answer: 'test answer', questionId: 1 })
+  await answerSurveyQuestion(makeApolloClient(), { answer: 'test answer', questionId: 1 })
+}
+
+function makeApolloClient() {
+  return new ApolloClient({
+    ssrMode: true,
+    link: new HttpLink({
+      uri: `https://cs188.cloudcity.computer/graphql`,
+      credentials: 'same-origin',
+      fetch: async (uri: any, options: any) => {
+        const reqBody = JSON.parse(options!.body! as string)
+        const opName = reqBody.operationName
+        const actionName = reqBody.variables?.action?.actionName
+        const headers = authToken ? { ...options.headers, 'x-authtoken': authToken } : options.headers
+        return fetch(`${uri}?opName=${opName}${actionName ? `&actionName=${actionName}` : ''}`, {
+          ...options,
+          headers,
+        })
+      },
+    }),
+    cache: new InMemoryCache(),
+  })
+}
